60f6cc8a0d461ab5a6a6c0c0536c13781f1d9c8b
Emine Shaban <emineshaban@icloud.com>
2025-04-25 14:08:47 +0300

    .

--- js/index.js
+++ js/index.js
@@ -11,10 +11,28 @@ navbar.addEventListener(("click"), (e) => {
         active.style.opacity = "1";
         bag.style.display = "none"
     } else if (menu.className == "menu active") {
-        menu.className = "menu" 
+        menu.className = "menu"
         bag.style.display = "block"
     }
 })
+
+let dropdownMenu = document.getElementsByClassName("dropdown-menu")[0];
+let menuProd = document.getElementsByClassName("menu-prod")[0];
+console.log(menuProd);
+
+menuProd.addEventListener(("click"), (e) => {
+    e.preventDefault();
+    if (dropdownMenu.className == "dropdown-menu") {
+        dropdownMenu.className = "dropdown-menu show"
+        dropdownMenu.style.display = "block"
+
+    } else {
+        dropdownMenu.className = "dropdown-menu"
+        dropdownMenu.style.display = "none"
+
+    }
+})
+
 
 
 
@@ -53,7 +71,7 @@ const observer2 = new IntersectionObserver(intersections => {
 });
 document.querySelectorAll('.banner-aft-div').forEach(div => {
     observer2.observe(div);
-});  
+});
 
 
 const observer3 = new IntersectionObserver(intersections => {
@@ -61,24 +79,24 @@ const observer3 = new IntersectionObserver(intersections => {
         target,
         isIntersecting
     }) => {
-        console.log(target.children)
+
         target.children[0].classList.toggle('slide-in-right', isIntersecting);
         target.children[1].classList.toggle('slide-in-top', isIntersecting);
-        target.children[2].classList.toggle('slide-in-left', isIntersecting); 
+        target.children[2].classList.toggle('slide-in-left', isIntersecting);
     });
 }, {
     threshold: 0
 });
 document.querySelectorAll('.typography-div').forEach(div => {
     observer3.observe(div);
-});  
+});
 
 
 const observer4 = new IntersectionObserver(intersections => {
     intersections.forEach(({
         target,
         isIntersecting
-    }) => {  
+    }) => {
         target.children[0].children[0].classList.toggle('expandIn-animation1', isIntersecting);
         target.children[1].classList.toggle('expandIn-animation2', isIntersecting);
     });
@@ -87,7 +105,7 @@ const observer4 = new IntersectionObserver(intersections => {
 });
 document.querySelectorAll('.product-imgs').forEach(div => {
     observer4.observe(div);
-});  
+});
 
 
 
@@ -97,22 +115,70 @@ document.querySelectorAll('.product-imgs').forEach(div => {
 const selectHotspot = (e) => {
     const clickedHotspot = e.target.parentElement;
     const container = clickedHotspot.parentElement;
-    
+
     // only include hotspots within same image to allow one open hotspot per image; change "container" to "document" to allow only one open hotspot for entire page:
-    const hotspots = container.querySelectorAll(".lg-hotspot"); 
+    const hotspots = container.querySelectorAll(".lg-hotspot");
     hotspots.forEach(hotspot => {
-      if (hotspot === clickedHotspot) {
-        hotspot.classList.toggle("lg-hotspot--selected");
-      } else {
-        hotspot.classList.remove("lg-hotspot--selected");
-      }
+        if (hotspot === clickedHotspot) {
+            hotspot.classList.toggle("lg-hotspot--selected");
+        } else {
+            hotspot.classList.remove("lg-hotspot--selected");
+        }
     });
-  }
-  
-  (() => {
+}
+
+(() => {
     const buttons = document.querySelectorAll(".lg-hotspot__button");
     buttons.forEach(button => {
-      button.addEventListener("click", selectHotspot);
+        button.addEventListener("click", selectHotspot);
     });
-  })();
-  
+})();
+
+
+
+
+
+var acc = document.getElementsByClassName("q");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+
+// Comments
+
+
+var comment = document.getElementsByClassName("comment");
+var viewMore = document.getElementsByClassName("viewMore");
+
+viewMore[0].addEventListener("click", function () {
+    buttonView();
+    for (let i = 3; i <= comment.length; i++) {
+        if (comment[i].style.display == "none" || comment[i].style.display == "") {
+            for (let index = 0; index <= 2; index++) {
+                if (comment[i + index] != undefined) {
+                    comment[i + index].style.display = "block";
+                } else {
+                    buttonView();
+                }
+            }
+            i += 3;
+            break
+        }
+    }
+})
+
+function buttonView() {
+    if (comment[comment.length - 1].style.display == "block") {
+        viewMore[0].style.display = "none";
+    }
+}
